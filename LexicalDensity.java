1456153c01d3f385af24010cef37feeba9741290
Adriano Teixeira <adrianodiasteixeira@gmail.com>
2017-12-19 23:42:27 +0000

    lexical density

---
+++ LexicalDensity.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lexicaldensity;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
+
+/**
+ *
+ * @author adt
+ */
+public class LexicalDensity {
+    
+    public static double calculate(String text) throws IOException{
+        
+        InputStream modelIn = new FileInputStream("C:\\Users\\adt\\Documents\\NetBeansProjects\\LexicalDensity\\en-pos-maxent.bin");
+        POSModel model = new POSModel(modelIn);
+        POSTaggerME tagger = new POSTaggerME(model);
+        ArrayList<String> tokenized = new ArrayList<>();
+        
+        String[] lexicalTags = new String[] {
+            "JJ","JJR","JJS","NN","NNS","NP","NPS","RB","RBR","RBS","VB","VBD",
+            "VBG","VBN","VBP","VBZ","VH","VHD","VHG","VHN","VHP","VHZ","VV","VVD",
+            "VVG","VVN","VVP","VVZ"
+        };
+        
+        StringTokenizer st = new StringTokenizer(text);
+        while (st.hasMoreTokens()) {
+            tokenized.add(st.nextToken());
+        }
+      
+        String[] textArray = tokenized.toArray(new String[0]);
+        
+        String[] tags = tagger.tag(textArray);
+        
+        double tokens = 0.0;
+       
+        for(String tag : tags)
+            if(Arrays.asList(lexicalTags).contains(tag)) tokens++;
+            //System.out.println(tag);
+
+        double totalWords = text.split(" ").length;
+        
+        double density = tokens/totalWords*100;
+        
+        return density;
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+LexicalDensity
+====================
+
+Antes de utilizar transferir o ficheiro <a href="http://opennlp.sourceforge.net/models-1.5/">en-pos-maxent.bin</a> para a máquina local
+e substituir o PATH na linha 25 do ficheiro LexicalDensity.java, pelo PATH correspondente.
